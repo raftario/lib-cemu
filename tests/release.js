a71c3b7682d33885c65d22dcc4eb16c5e2341ffd
Raphaël Thériault <raphael_theriault@outlook.com>
2019-04-16 10:19:59 -0400

    Fixed tests

--- tests/release.js
+++ tests/release.js
@@ -3,10 +3,10 @@
 const cemu = require('../lib')
 
 module.exports = () => {
-  let cemu_1_15_4 = cemu.release.create('1.15.4')
-  console.log(cemu_1_15_4)
+  let cemu1154 = cemu.release.create('1.15.4')
+  console.log(cemu1154)
 
-  cemu.release.download(cemu_1_15_4, './temp/cemu.zip', err => {
+  cemu.release.download(cemu1154, './temp/cemu.zip', err => {
     if (err) console.error(err)
     console.log('download callback')
   })
